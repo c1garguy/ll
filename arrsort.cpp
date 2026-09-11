abef3fb889b8b620cdca69ee95eef891543bd800
c1garguy <wakeupontheotherside@gmail.com>
2026-09-11 22:19:06 +0400

    Implement array manipulation functions in arrsort.cpp
    This file contains functions to create, assign, double, find the biggest element, swap, and reverse an array. The main function demonstrates the usage of these array operations.

---
+++ arrsort.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+int* createarr(const int size) {
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		*(arr + i) = 0;
+	}
+	return arr;
+}
+void assigncor(int* arr, const int size) {
+	for (int i{}; i < size; i++) {
+		*(arr + i) = i + 1;
+		std::cout << *(arr + i) << " ";
+	}
+	std::cout << std::endl;
+}
+void doublearr(int* arr, const int size) {
+	for (int i{}; i < size; i++) {
+		*(arr + i) = *(arr + i) * 2;
+		std::cout << *(arr + i) << " ";
+	}
+	std::cout << std::endl;
+}
+int findbiggest(int* arr, const int size) {
+	int first = *arr;
+	for (int i{ 1 }; i < size; i++) {
+		if (*(arr + i) > first) {
+			first = *(arr + i);
+		}
+	}
+	return first;
+}
+void swap(int& a, int& b) {
+	int c = a;
+	a = b;
+	b = c;
+}
+void reversearr(int* arr, const int size) {
+	int j = 0;
+	for (int i = size - 1; i >= 0; i--) {
+		if (j == i) {
+			break;
+		}
+		swap(*(arr + i), *(arr + j));
+		j++;
+	}
+	for (int i = 0; i < size; i++) {
+		std::cout << *(arr + i) << " ";
+	}
+	std::cout << std::endl;
+}
+int main() {
+	int size;
+  std::cin >> size;
+	int* arr = createarr(size);
+	assigncor(arr, size);
+	doublearr(arr, size);
+	std::cout<<findbiggest(arr, size);
+	std::cout << std::endl;
+	reversearr(arr, size);
+	delete[] arr;
+	arr = nullptr;
+	return 0;
+}
